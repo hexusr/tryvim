73e6686e5eee9989eb293077d4d55836a0f20f59
hexusr <31689247+hexusr@users.noreply.github.com>
2018-01-30 11:31:58 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,8 @@
-: uninstall.sh ⮀                                                                                          ⮂⮂ [buffers] 
-  1 #! /usr/bin/bash
-  2 
-  3 rm -rf ~/.vimrc
-  4 rm -rf ~/.vim
-  5 cd ~/
-  6 rm -rf ~/tryvim
-  7 
-  8 echo "卸载完毕!"
-~                     
+#! /usr/bin/bash
+  
+rm -rf ~/.vimrc
+rm -rf ~/.vim
+cd ~/
+rm -rf ~/tryvim
+
+8 echo "卸载完毕!"                     
